b722304901c4a689d95a4a8ec842cacd76260fc8
Nishanth v <100022076+nishanth-code@users.noreply.github.com>
2022-12-30 11:33:02 +0000

    restart

--- frst_project/src/app/app.component.spec.ts
+++ frst_project/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'demo'`, () => {
+  it(`should have as title 'frst_project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('demo');
+    expect(app.title).toEqual('frst_project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('demo app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('frst_project app is running!');
   });
 });
